be419aa61f5563a5df1a38be68ff2bb6ac52559b
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-10-21 21:47:19 +0200

    FIRST working code

---
+++ index.js
@@ -0,0 +1,3 @@
+const validator = require("./validator");
+
+module.exports = validator;
---
+++ test/index.js
@@ -0,0 +1,4 @@
+const Validator = require('../index')
+const validator = new Validator('20035081P')
+
+console.log(validator.validateSpanishId())
