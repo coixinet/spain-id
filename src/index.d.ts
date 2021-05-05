bd8ee9544da52799613d1ef395b16bd6617454f0
Tim Geisler <tg@webxcerpt.com>
2021-05-05 12:18:31 +0200

    export validDNI in typescript module definition
    validDNI was missing from the exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,5 +2,6 @@ declare module 'spain-id' {
     export const validateSpanishId: (x: string) => boolean
     export const spainIdType: (x: string) => string
     export const validNIE: (x: string) => boolean
+    export const validDNI: (x: string) => boolean
     export const validCIF: (x: string) => boolean
 }
